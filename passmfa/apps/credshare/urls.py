ebae9b68dbb26aa9b089b7f38bb97842216758c4
winston <wenbinhk@gmail.com>
2024-07-30 12:19:14 +0800

    完成share

--- passmfa/apps/credshare/urls.py
+++ passmfa/apps/credshare/urls.py
@@ -8,23 +8,32 @@ urlpatterns = [
     # Other URL patterns...
 
     #path("test1/",Test_View.as_view(template_name="index4.html"),),
-    path('api/get_otp/<int:credential_id>/', get_otp, name='get_2fa_code'),
+    path('otp/<int:credential_id>/', get_otp, name='get_otp'),
     #path("otp/",IndexView.as_view(template_name="otp.html"),name='otp'),
+
+    #用户首页
     path("index/",IndexView.as_view(template_name="index.html"),name='index'),
 
     #path("index3/",IndexView.as_view(template_name="index3.html"),name='index3'),
     #path("index2/",IndexView.as_view(template_name="ui_modals.html"),name='index2'),
+    #登录出页
     path("login/",LoginView.as_view(template_name="login.html"),name='login'),
     path("logout/",userlogout,name='userlogout'),
 
+    #读取qrcode图片
     path('qrcode/', qrcode, name='qrcode'),
 
+    #添加页
     path("new/",NewView.as_view(template_name="new.html"),name='new'),
-    path("ed/<int:id>/",EditView.as_view(template_name="edit.html"),name='edit'),
+    #修改页
+    path("edit/<int:id>/",EditView.as_view(template_name="edit.html"),name='edit'),
+
+    #共享给我的
+    path('share/', SharetomeView.as_view(template_name="sharetome.html"), name='sharetome'),
 
     path('list/', credentials_list, name='credentials_list'),
     path('add/', add_credentials, name='add_credentials'),
-    path('edit/<int:id>/', edit_credentials, name='edit_credentials'),
+    #path('edit/<int:id>/', edit_credentials, name='edit_credentials'),
 
 
     path('delete/<int:id>/', delete_credentials, name='delete_credentials'),
@@ -35,7 +44,6 @@ urlpatterns = [
     path('delshare/<int:type>/<int:id>/', delshare_credentials, name='delshare_credentials'),
     #path('getshare/<int:id>/', getshare_credentials, name='getshare_credentials'),
     #get shared credentials
-    path('share/', SharetomeView.as_view(template_name="sharetome.html"),name='sharetome'),
     #path("register/",ProfileView.as_view(template_name="profile.html"),),
     path('404/', testView.as_view(template_name="404.html"),name='404'),
 
